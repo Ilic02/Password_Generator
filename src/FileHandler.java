46c1f4a314501af06a4c0ee6c3752311bfa3fbe3
Ilic02 <nikola.ni959@gmail.com>
2025-02-13 21:36:05 +0100

    Edit FileHandler

--- src/FileHandler.java
+++ src/FileHandler.java
@@ -5,12 +5,23 @@ import java.time.format.DateTimeFormatter;
 public class FileHandler {
     private static final String FILE_PATH = "src/Passwords.txt";
 
+    private static void ensureFileExists() throws IOException {
+        File file = new File(FILE_PATH);
+        if (!file.exists()) {
+            file.getParentFile().mkdirs();
+            file.createNewFile();
+        }
+    }
+
     public static boolean siteExists(String website) {
-        try (BufferedReader reader = new BufferedReader(new FileReader(FILE_PATH))) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                if (line.contains("Website: " + website))
-                    return true;
+        try {
+            ensureFileExists();
+            try (BufferedReader reader = new BufferedReader(new FileReader(FILE_PATH))) {
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    if (line.contains("Website: " + website))
+                        return true;
+                }
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -19,6 +30,8 @@ public class FileHandler {
     }
 
     public static void savePassword(String website, String password) throws IOException {
+        ensureFileExists();
+
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
         LocalDateTime time = LocalDateTime.now();
 
